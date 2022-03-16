a48cd59f4b56bacc056150fb270ca65838140daf
Gedeon Nyengele <thenextged@gmail.com>
2022-03-16 16:56:34 -0700

    added command-line arguments

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# axi2sram
-AXI4 to SRAM Interface
+# AXI4 to SRAM Interface Converter
+
+This utility converter helps connect a device with an SRAM interface to an
+AXI4 bus/interface
+
+## Features
+
+This interface converter is parameterized over:
+- the size AXI ID ports
+- the size of the address ports
+- the size of the data ports
+
+It also:
+- supports for write byte enable signals
+- single-port SRAM interface
+
+## Chisel Generation
+
+To find all supported command-line arguments, run `sbt 'run --help'` in the top-level directory
+
+To generate, for example, an interface converter with AXI ID widths of 4 bits, 32-bit address busses, and 64-bit data busses:
+- `sbt 'run -i 4 -a 32 -d 64 -o <OUTPUT_DIR_PATH>'`
+- or `sbt 'run --id-width 4 --address-width 32 --data-width 64 --output-dir <OUTPUT_DIR_PATH>'`
--- src/main/scala/Top.scala
+++ src/main/scala/Top.scala
@@ -1,18 +1,65 @@
 package aha
 package axi2sram
 
+/* Scala Imports */
+import scopt.OParser
+
+/* Chisel Imports */
 import chisel3._
 import chisel3.stage.ChiselStage
 
+/*
+ * Configuration
+ */
+case class GenConfig (
+    // Width of AXI ID Signals
+    IdWidth     : Int       = 4,
+    // Width of Address Busses (in bits)
+    AddrWidth   : Int       = 32,
+    // Width of Data Busses (in bits)
+    DataWidth   : Int       = 64,
+    // Output Directory
+    OutputDir   : String    = "output/"
+)
+
 object Main extends App {
 
-    val IdWidth         = 4
-    val AddrWidth       = 32
-    val DataWidth       = 64
-    (new ChiselStage).emitVerilog(
-        new AXItoSRAM(  IdWidth,
-                        AddrWidth,
-                        DataWidth),
-        Array("--target-dir", "output/")
-    )
+
+    val builder = OParser.builder[GenConfig]
+    val parser  = {
+        import builder._
+        OParser.sequence (
+            programName("axi2sram"),
+            head("axi2sram", "1.0"),
+
+            opt[Int]('i', "id-width")
+                .action((x, c) => c.copy(IdWidth = x))
+                .text("width of AXI ID signals"),
+
+            opt[Int]('a', "address-width")
+                .action((x, c) => c.copy(AddrWidth = x))
+                .text("width of address busses"),
+
+            opt[Int]('d', "data-width")
+                .action((x, c) => c.copy(DataWidth = x))
+                .text("width of data busses"),
+
+            opt[String]('o', "output-dir")
+                .action((x, c) => c.copy(OutputDir = x))
+                .text("output directory")
+        )
+    }
+
+    OParser.parse(parser, args, GenConfig()) match {
+        case Some(config) => new ChiselStage().emitVerilog(
+            new AXItoSRAM (
+                config.IdWidth,
+                config.AddrWidth,
+                config.DataWidth
+            ),
+            Array("--target-dir", config.OutputDir)
+        )
+
+        case _ => ()
+    }
 }
